fb200a8bee033f02f383d3faca26bc6232f9bb78
Theresa Anyadike <nnennaanyadike@gmail.com>
2021-03-29 13:34:56 +0100

    title changed

--- src/components/card/cardComponent.js
+++ src/components/card/cardComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './cardComponent.css'
 
-export const Card = (props) => {
+export const Card = props => {
     return <div className='card-container'>
-        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+        <img alt='monster' src={`https://robohash.org/$
+        {props.monster.id}?set=set2&size=180x180`} />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
 </div>
