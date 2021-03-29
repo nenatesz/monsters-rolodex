949dbbe0d37b9fb042a0d67d64d28a7fb5a4d561
Theresa Anyadike <nnennaanyadike@gmail.com>
2021-03-29 13:36:33 +0100

    fixed issue with the image

--- src/components/card/cardComponent.js
+++ src/components/card/cardComponent.js
@@ -3,8 +3,7 @@ import './cardComponent.css'
 
 export const Card = props => {
     return <div className='card-container'>
-        <img alt='monster' src={`https://robohash.org/$
-        {props.monster.id}?set=set2&size=180x180`} />
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
 </div>
